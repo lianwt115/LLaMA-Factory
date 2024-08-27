c867ed04b2f83468f9c9b522f9aafa2486e7298c
lianwentao <lianwentao@ctdcn.com>
2024-08-27 09:41:17 +0800

    lwt

---
+++ downloadModel.py
@@ -0,0 +1,8 @@
+#SDK模型下载
+from modelscope import snapshot_download
+#model_dir0 = snapshot_download('OpenBuddy/openbuddy-llama3.1-8b-v22.1-131k')
+#model_dir2 = snapshot_download('Qwen/Qwen2-7B-Instruct-AWQ')
+model_dir1 = snapshot_download('Qwen/Qwen2-7B-Instruct',cache_dir='./')
+#model_dir3 = snapshot_download('Qwen/Qwen2-1.5B')
+#训练模型  使用qlora 训练
+#model_dir = snapshot_download('Qwen/Qwen2-7B-Instruct-GPTQ-Int4')
